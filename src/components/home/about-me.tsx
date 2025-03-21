c5a362a9a4951fa3a498cd455c58cf9aa60f928c
José Carlos de Freitas <josecfreitas@outlook.com.br>
2025-03-21 10:34:14 -0300

    refactor about-me section for clarity and conciseness

--- src/components/home/about-me.tsx
+++ src/components/home/about-me.tsx
@@ -6,28 +6,27 @@ export const AboutMe = () => (
   <Section>
     <SectionTitle>About me</SectionTitle>
     <Text>
-      Greetings! I'm a Brazilian programmer with over a decade of experience in
-      computer science. I began studying programming at the age of 15 in 2011,
-      and since then, I've achieved an impressive academic background, including
-      an associate's, bachelor's, and master's degrees in computer science.
-      Additionally, I've had the privilege of publishing two articles in
-      international events, demonstrating my expertise in the field.
+      I'm a software engineer with over a decade of experience in computer
+      science. I continuously strive for excellence in my career. I started
+      programming at 14 in 2011 and have since built an impressive academic
+      foundation, earning associate’s, bachelor’s, and master’s degrees in
+      computer science. Along the way, I published two articles at international
+      conferences, showcasing my expertise.
     </Text>
     <Text>
-      Throughout my career, I've completed over 200 courses on Alura, a tech
-      course platform, and successfully delivered 30+ end-to-end projects
-      ranging from Landing Pages to Apps and Systems. My passion for computer
-      science drives me to continuously learn, with a particular interest in
-      DevOps.
+      My dedication to learning and growth has led to rapid career progression.
+      I earned promotions almost yearly and achieved Staff Software Engineer 3
+      before age 30. Known for being highly proactive, I consistently deliver
+      value fast, driving impactful results across teams and projects. I've
+      completed over 200 courses on Alura and successfully delivered many
+      end-to-end projects, ranging from landing pages to complex applications
+      and systems.
     </Text>
     <Text>
-      I genuinely enjoy solving complex problems with technology, and it is this
-      passion that motivates me to stay up-to-date with the latest advancements
-      in the field.
-    </Text>
-    <Text>
-      Don't hesitate to get in touch, and I look forward to the opportunity to
-      discuss how I can contribute to your team.
+      My dedication to engineering excellence and developer experience has
+      consistently raised the standard for every team I've been part of. I'm
+      always eager to tackle new challenges, innovate, and push the boundaries
+      of what's possible in technology.
     </Text>
   </Section>
 );
